609acd7dffdbfd963fb17b35f953cdbdb7163839
zonesama <trannhatthanh31@gmail.com>
2019-11-22 09:01:14 +0700

    First Commit.

---
+++ src/main/java/config/ApplicationInitializer.java
@@ -0,0 +1,23 @@
+package config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebSecurityConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { ApplicationConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
